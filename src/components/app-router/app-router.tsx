88c7ba323c1da4f9c8f52b1b6da57d542ad1ebb6
CarlosRuiz <carua97@gmail.com>
2023-03-14 18:00:30 +0100

    Add app router and test

---
+++ src/components/app-router/app-router.tsx
@@ -0,0 +1,23 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { MenuOption } from "../app/app";
+
+const Home = lazy(() => import("../home/home"));
+const Profile = lazy(() => import("../profile/profile"));
+
+type AppRouterProps = {
+  menuOptions: MenuOption[];
+};
+
+export function AppRouter({ menuOptions }: AppRouterProps) {
+  return (
+    <Suspense>
+      <Routes>
+        <Route path={"/"} element={<Home></Home>}></Route>
+        <Route path={menuOptions[0].path} element={<Home></Home>}></Route>
+        <Route path={menuOptions[1].path} element={<Profile></Profile>}></Route>
+        {/* Ruta para la pagina de error */}
+      </Routes>
+    </Suspense>
+  );
+}
