f0c5f6718b116e6b36361be54b728b3c1000142c
CarlosRuiz <carua97@gmail.com>
2023-03-14 18:02:13 +0100

    Fix eslint test

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from "./header";
 
-describe("Given the Header component ", () => {
+describe("Given the Header component", () => {
   describe("when you enter in the component", () => {
     test("then it should render it", () => {
       render(
