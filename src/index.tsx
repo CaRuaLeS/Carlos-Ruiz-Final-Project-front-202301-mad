c16d253c2b87a792860ffc95bacd1bd24fc1472c
CarlosRuiz <carua97@gmail.com>
2023-03-14 14:37:34 +0100

    Add app import to index

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { store } from "./store/store";
-import App from "./components/app/App";
+import App from "./components/app/app";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
