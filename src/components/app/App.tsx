a7c316fd22a43d07cd23138876045198a234bfed
CarlosRuiz <carua97@gmail.com>
2023-03-14 18:00:48 +0100

    Add app component and test

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { AppRouter } from "../app-router/app-router";
+import { Header } from "../header/header";
+import { Menu } from "../menu/menu";
 import "./app.css";
 
+export type MenuOption = {
+  label: string;
+  path: string;
+  image?: string;
+};
+export const menuOptions: MenuOption[] = [
+  { label: "Home", path: "/home", image: "./images/home_icon.svg" },
+  { label: "Profile", path: "/profile", image: "./images/profile_icon.svg" },
+];
+
 function App() {
-  return <div className="App">Start project</div>;
+  return (
+    <>
+      <Header>
+        <Menu options={menuOptions}></Menu>
+      </Header>
+      <AppRouter menuOptions={menuOptions}></AppRouter>
+    </>
+  );
 }
 
 export default App;
