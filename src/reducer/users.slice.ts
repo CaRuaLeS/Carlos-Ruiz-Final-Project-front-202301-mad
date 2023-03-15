0323d59400da344c0bbdb31e9dc32f2dddd4744f
CarlosRuiz <carua97@gmail.com>
2023-03-15 11:18:05 +0100

    Add user slice feature

---
+++ src/reducer/users.slice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserStructure } from "../model/user";
+
+export type State = {
+  userLogged: UserStructure;
+  users: UserStructure[];
+};
+
+const initialState: State = {
+  userLogged: {} as UserStructure,
+  users: [],
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    register(state, action: PayloadAction<UserStructure>) {
+      state.users = [...state.users, action.payload];
+    },
+    login(state, action: PayloadAction<UserStructure>) {
+      state.userLogged = action.payload;
+    },
+  },
+});
