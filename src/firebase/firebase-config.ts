e02f1ed6ae08320529774dc3cce23aee0757c4c2
CarlosRuiz <carua97@gmail.com>
2023-03-16 18:19:40 +0100

    Add firebase to env for vulnerability

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCgdVYZ5un1ZLvcgn5H7_PG5h8WeXS4nAM",
+  apiKey: process.env.FB_APIKEY,
   authDomain: "add-image-9a3cd.firebaseapp.com",
   databaseURL:
     "https://add-image-9a3cd-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "add-image-9a3cd",
   storageBucket: "add-image-9a3cd.appspot.com",
-  messagingSenderId: "754681433404",
-  appId: "1:754681433404:web:f415a1fa4c6962ad972239",
-  measurementId: "G-GQHS4PD0TW",
+  messagingSenderId: process.env.FB_MSID,
+  appId: process.env.FB_APPID,
+  measurementId: process.env.FB_MEASUREID,
 };
