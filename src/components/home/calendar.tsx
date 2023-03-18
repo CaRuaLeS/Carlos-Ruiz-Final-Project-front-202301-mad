8405b970252c2308bde254189f174fa5740c31a3
Carlos <carua97@gmail.com>
2023-03-18 23:35:32 +0100

    Add calendar first view

---
+++ src/components/home/calendar.tsx
@@ -0,0 +1,74 @@
+interface Props {
+  monthOffset: number;
+}
+
+export function Calendar({ monthOffset }: Props) {
+  const now = new Date();
+  const yearOffset = monthOffset / 12;
+  const normalizedMonthOffset = monthOffset % 12;
+  const firstOfMonth = new Date(
+    now.getFullYear() + yearOffset,
+    now.getMonth() + normalizedMonthOffset,
+    1
+  );
+  const lastOfMonth = new Date(
+    now.getFullYear() + yearOffset,
+    now.getMonth() + normalizedMonthOffset + 1,
+    0
+  );
+  const firstWeekDayOfMonth = firstOfMonth.getDay();
+  const lastWeekDayOfMonth = lastOfMonth.getDay();
+  console.log(firstWeekDayOfMonth);
+  console.log(lastWeekDayOfMonth);
+  const offset = -firstWeekDayOfMonth;
+  const rows = [];
+  for (let i = 0; i < 6; i++) {
+    rows.push(CalendarWeek(i, offset, lastOfMonth));
+  }
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+        </tr>
+      </thead>
+      {rows}
+    </table>
+  );
+}
+
+export function CalendarWeek(week: number, offset: number, lastOfMonth: Date) {
+  const days = [];
+  for (let i = 0; i < 7; i++) {
+    days.push(CalendarDay(week * 7 + i + offset + 1, lastOfMonth));
+  }
+
+  return <tr>{days}</tr>;
+}
+
+export function CalendarDay(day: number | string, lastOfMonth: Date) {
+  if (day < 1) day = "";
+  if (day > lastOfMonth.getDate()) day = "";
+  console.log(
+    `${day}/${lastOfMonth.getMonth() + 1}/${lastOfMonth.getFullYear()}`
+  );
+  return (
+    <td>
+      <button
+        onClick={() =>
+          console.log(
+            `${day}/${lastOfMonth.getMonth() + 1}/${lastOfMonth.getFullYear()}`
+          )
+        }
+      >
+        {day}
+      </button>
+    </td>
+  );
+}
