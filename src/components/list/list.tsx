181605a9bc7a546c8a9ab19fd2d7657b73b15170
Carlos <carua97@gmail.com>
2023-03-18 15:15:41 +0100

    Add list estructure

---
+++ src/components/list/list.tsx
@@ -0,0 +1,9 @@
+export function List() {
+  // Use effect with all the escaperooms
+  return (
+    <section className="list">
+      <div>list</div>
+      <ul className="list__ul">{/* { Map with each card } */}</ul>
+    </section>
+  );
+}
