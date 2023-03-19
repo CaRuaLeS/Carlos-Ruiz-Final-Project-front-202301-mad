ec6de90255da846f723b031a603d6cee4ed8756b
Carlos <carua97@gmail.com>
2023-03-19 12:32:30 +0100

    Add useEscaperoom hook and test

---
+++ src/hooks/use-escaperoom.test.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable testing-library/no-render-in-setup */
+/* eslint-disable testing-library/no-unnecessary-act */
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event/";
+import { Provider } from "react-redux";
+import { EscaperoomsRepo } from "../services/escaperoom-repo";
+import { store } from "../store/store";
+import { useEscapeRooms } from "./use-escaperooms";
+
+describe("Given the useEscaperooms hook", () => {
+  let mockRepo: EscaperoomsRepo;
+
+  beforeEach(async () => {
+    mockRepo = {
+      create: jest.fn(),
+    } as unknown as EscaperoomsRepo;
+
+    mockRepo = {
+      getByTheme: jest.fn(),
+      getAll: jest.fn(),
+    } as unknown as EscaperoomsRepo;
+
+    const TestComponent = function () {
+      const { escaperoomGetAll, escaperoomGetByTheme } =
+        useEscapeRooms(mockRepo);
+
+      return (
+        <>
+          <button onClick={() => escaperoomGetAll()}>getAll</button>
+          <button onClick={() => escaperoomGetByTheme("test")}>
+            getByTheme
+          </button>
+        </>
+      );
+    };
+
+    await act(async () =>
+      render(
+        <Provider store={store}>
+          <TestComponent></TestComponent>
+        </Provider>
+      )
+    );
+  });
+
+  describe("when Testcomponent renders", () => {
+    test("then buttons should be in the document", async () => {
+      const elements = await screen.findAllByRole("button");
+      expect(elements[0]).toBeInTheDocument();
+      expect(elements[1]).toBeInTheDocument();
+    });
+  });
+  describe("when escaperoomGetAll of the test component is called", () => {
+    test("then the getAll of the repo should be called", async () => {
+      const elements = await screen.findAllByRole("button");
+      await act(async () => userEvent.click(elements[0]));
+      expect(mockRepo.getAll).toHaveBeenCalled();
+    });
+  });
+  describe("when escaperoomGetByTheme of the test component is called", () => {
+    test("then the getByTheme of the repo should be called", async () => {
+      const elements = await screen.findAllByRole("button");
+      await act(async () => userEvent.click(elements[1]));
+      expect(mockRepo.getByTheme).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/hooks/use-escaperooms.ts
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
+import { EscapeRoomStructure } from "../model/escaperoom";
+import {
+  getAllEscaperooms,
+  getByThemeEscaperooms,
+} from "../reducer/escaperooms-slice";
+import { EscaperoomsRepo } from "../services/escaperoom-repo";
+import { AppDispatch, RootState } from "../store/store";
+
+export function useEscapeRooms(repo: EscaperoomsRepo) {
+  const escaperooms = useSelector((state: RootState) => state.escaperooms);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const escaperoomGetAll = async () => {
+    try {
+      const data = await repo.getAll();
+      dispatch(getAllEscaperooms(data.results[0]));
+    } catch (error) {
+      console.error((error as Error).message);
+    }
+  };
+  const escaperoomGetByTheme = async (theme: EscapeRoomStructure["theme"]) => {
+    try {
+      const data = await repo.getByTheme(theme);
+      dispatch(getByThemeEscaperooms(data.results[0]));
+    } catch (error) {
+      console.error((error as Error).message);
+    }
+  };
+
+  return {
+    escaperooms,
+    escaperoomGetAll,
+    escaperoomGetByTheme,
+  };
+}
