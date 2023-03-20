2f9f1a46f63d748cf78f6cf5b9c159b22df449ba
Carlos <carua97@gmail.com>
2023-03-20 19:59:58 +0100

    Add function filter to home

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,11 +1,27 @@
+import { useMemo, useState } from "react";
+import { useEscapeRooms } from "../../hooks/use-escaperooms";
+import { EscaperoomsRepo } from "../../services/escaperoom-repo";
 import { List } from "../list/list";
 
 export function Home() {
+  const repo = useMemo(() => new EscaperoomsRepo(), []);
+  const { escaperoomGetAll, escaperoomGetByTheme } = useEscapeRooms(repo);
+
   return (
     <section className="home">
       <h2 className="home__title">Home</h2>
-      <select className="home__select">
-        <option value="All" placeholder="Select a theme"></option>
+      <select
+        onChange={async (element) => {
+          const selectedTheme = element.target.value;
+          selectedTheme === "All"
+            ? escaperoomGetAll()
+            : escaperoomGetByTheme(selectedTheme);
+        }}
+        className="home__select"
+      >
+        <option value="All" placeholder="Select a theme">
+          All
+        </option>
         <option value="Fantasy">Fantasy</option>
         <option value="Mistery">Mistery</option>
         <option value="Medieval">Medieval</option>
