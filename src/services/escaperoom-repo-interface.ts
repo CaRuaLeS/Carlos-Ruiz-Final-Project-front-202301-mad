6024a8d7f4778ab8ad959b1fc27d5bb458170e15
CarlosRuiz <carua97@gmail.com>
2023-03-20 16:46:43 +0100

    Add readby Id repo function and interface

--- src/services/escaperoom-repo-interface.ts
+++ src/services/escaperoom-repo-interface.ts
@@ -2,5 +2,6 @@ import { EscapeRoomStructure } from "../model/escaperoom";
 
 export interface RepoEscaperoom<T> {
   getAll(): Promise<T>;
+  getById(roomId: string): Promise<T>;
   getByTheme(themeFilter: EscapeRoomStructure["theme"]): Promise<T>;
 }
