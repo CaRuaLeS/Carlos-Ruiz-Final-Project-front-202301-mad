7324237157dcbc69cb49cf6255fb2fd598daece5
Carlos <carua97@gmail.com>
2023-03-25 23:17:13 +0100

    Profile test, almost 100%, oine line!!!

--- src/components/profile/profile.test.tsx
+++ src/components/profile/profile.test.tsx
@@ -1,34 +1,63 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
 import { fireEvent, render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "../../store/store";
-import { Register } from "../register/register";
+import { useDispatch, useSelector } from "react-redux";
+import { LoggedAccount } from "../profile-logged/logged";
+import NotLogged from "../profile-not-logged/not-logged";
 import Profile from "./profile";
 
-jest.mock("../register/register");
-
-beforeEach(() => {
-  render(
-    <Provider store={store}>
-      <Profile></Profile>
-    </Provider>
-  );
-});
+jest.mock("../profile-not-logged/not-logged");
+jest.mock("../profile-logged/logged");
+jest.mock("react-redux", () => ({
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(),
+}));
 
 describe("Given the profile component", () => {
-  describe("when we render it", () => {
-    test("then register have benn called", () => {
-      expect(Register).toHaveBeenCalled();
+  let handlerLogout: any;
+  let usersMock;
+
+  beforeEach(() => {
+    (useDispatch as jest.Mock).mockReturnValue(handlerLogout);
+
+    usersMock = {
+      extraInfo: {
+        token: undefined,
+      },
+    };
+    (useSelector as jest.Mock).mockReturnValue(usersMock);
+  });
+  afterEach(() => jest.clearAllMocks());
+
+  describe("when there IS NO TOKEN in the state", () => {
+    test("then the NotLogged component is rendered", () => {
+      render(<Profile />);
+      expect(NotLogged).toHaveBeenCalled();
     });
   });
-  describe("when we check the buttons", () => {
-    test("then they should be in the document", () => {
-      const button = screen.getAllByRole("button");
+  describe("when there IS TOKEN in the state", () => {
+    beforeEach(() => {
+      handlerLogout = jest.fn();
+      (useDispatch as jest.Mock).mockReturnValue(handlerLogout);
 
-      fireEvent.click(button[0]);
-      fireEvent.click(button[1]);
-      expect(button[0]).toBeInTheDocument();
-      expect(button[1]).toBeInTheDocument();
+      usersMock = {
+        extraInfo: {
+          token: "123456",
+        },
+      };
+      (useSelector as jest.Mock).mockReturnValue(usersMock);
+    });
+    test("then the LoggedAccount component is rendered", () => {
+      render(<Profile />);
+      expect(LoggedAccount).toHaveBeenCalled();
+    });
+  });
+  describe("when the logut button is clicked", () => {
+    test("then logout handler should be called", () => {
+      render(<Profile />);
+      const element = screen.getByRole("button");
+      fireEvent.click(element);
+      expect(handlerLogout).toHaveBeenCalled();
     });
   });
 });
