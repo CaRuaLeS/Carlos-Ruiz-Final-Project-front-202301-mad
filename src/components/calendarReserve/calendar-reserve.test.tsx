7ed595ebde5ee33a44e25eda5d9b6dcc6c1e0ee3
Carlos <carua97@gmail.com>
2023-03-25 23:17:41 +0100

    Add calendar reserve functions with token

--- src/components/calendarReserve/calendar-reserve.test.tsx
+++ src/components/calendarReserve/calendar-reserve.test.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
 import { act, fireEvent, render, screen } from "@testing-library/react";
+import { Provider, useSelector } from "react-redux";
 import { useReservations } from "../../hooks/use-reservations";
+import { store } from "../../store/store";
 import { ReserveInfo } from "../calendar/calendar";
 import { CalendarReserve } from "./calendar-reserve";
 
 jest.mock("../../hooks/use-reservations");
+jest.mock("react-redux", () => ({
+  useSelector: jest.fn(),
+}));
 
 describe("Given the CalendarReserve", () => {
   const mockReservationCreate = jest.fn();
@@ -15,10 +20,19 @@ describe("Given the CalendarReserve", () => {
     user: "098",
   } as ReserveInfo;
 
+  let usersMock;
+
   beforeEach(async () => {
     (useReservations as jest.Mock).mockReturnValue({
       reservationCreate: mockReservationCreate,
     });
+
+    usersMock = {
+      extraInfo: {
+        token: undefined,
+      },
+    };
+    (useSelector as jest.Mock).mockReturnValue(usersMock);
 
     await act(async () => {
       render(<CalendarReserve reservation={mockReservation} />);
@@ -30,6 +44,13 @@ describe("Given the CalendarReserve", () => {
       const element = screen.getByRole("button");
       fireEvent.click(element);
       expect(spy).toHaveBeenCalled();
+    });
+  });
+  describe("when you press the button", () => {
+    test("then it should called the reservationCreate function", () => {
+      const element = screen.getByRole("button");
+      fireEvent.click(element);
+      expect(mockReservationCreate).toHaveBeenCalled();
     });
   });
 });
