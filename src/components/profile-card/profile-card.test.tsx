34c92ff1da89cc4d2425fb6f0d2b7b53892424ad
Carlos <carua97@gmail.com>
2023-03-26 17:00:07 +0200

    Fix profile card test

--- src/components/profile-card/profile-card.test.tsx
+++ src/components/profile-card/profile-card.test.tsx
@@ -1,10 +1,23 @@
 /* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/no-unnecessary-act */
-import { act, render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import { Provider, useSelector } from "react-redux";
+import { useReservations } from "../../hooks/use-reservations";
 import { store } from "../../store/store";
 import { ProfileCard } from "./profile-card";
 
+jest.mock("react-redux", () => ({
+  useSelector: jest.fn(),
+}));
+jest.mock("../../hooks/use-reservations");
+
+const mockReservationDelete = jest.fn();
+
+const mockToken = {
+  extraInfo: {
+    token: "tokenTest",
+  },
+};
 const mockReserve = {
   name: "name test",
   escaperoom: {
@@ -14,12 +27,14 @@ const mockReserve = {
 
 describe("Given the card component", () => {
   beforeEach(async () => {
+    (useSelector as jest.Mock).mockReturnValue(mockToken);
+
+    (useReservations as jest.Mock).mockReturnValue({
+      reservationDelete: mockReservationDelete,
+    });
+
     await act(async () => {
-      render(
-        <Provider store={store}>
-          <ProfileCard reserves={mockReserve}></ProfileCard>
-        </Provider>
-      );
+      render(<ProfileCard reserves={mockReserve}></ProfileCard>);
     });
   });
 
@@ -27,6 +42,13 @@ describe("Given the card component", () => {
     test("then it should contain the list role", () => {
       const elements = screen.getAllByRole("listitem");
       expect(elements[0]).toBeInTheDocument();
+    });
+  });
+  describe("when the we press the handlerDelete", () => {
+    test("then it should call the resevationDelete", () => {
+      const element = screen.getByRole("button");
+      fireEvent.click(element);
+      expect(mockReservationDelete).toHaveBeenCalled();
     });
   });
 });
