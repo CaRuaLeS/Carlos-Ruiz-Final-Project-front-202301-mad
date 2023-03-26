2dfcce05c43cd1c86e00c433d7a2354d91bf7d03
Carlos <carua97@gmail.com>
2023-03-26 13:15:50 +0200

    Add to profile delete option

--- src/components/profile-card/profile-card.tsx
+++ src/components/profile-card/profile-card.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useReservations } from "../../hooks/use-reservations";
+import { ReservationsRepo } from "../../services/reservation-repo";
+import { RootState } from "../../store/store";
+
 type ProfileProps = {
   reserves: any;
 };
 
 export function ProfileCard({ reserves }: ProfileProps) {
+  const users = useSelector((state: RootState) => state.users);
+
+  const repo = new ReservationsRepo();
+  const { reservationDelete } = useReservations(repo);
+
+  const handleDelete = () => {
+    reservationDelete(reserves.id, users.extraInfo.token!);
+  };
+
   return (
     <li className="reserves-card">
       <div>{reserves.escaperoom.name}</div>
       <div>{reserves.reserveDate}</div>
+      <button onClick={handleDelete}>Delete</button>
     </li>
   );
 }
