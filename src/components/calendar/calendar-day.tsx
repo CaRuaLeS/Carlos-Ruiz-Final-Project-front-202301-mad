b060b4a90fe462b56de7cf69613c18af67a118f0
Carlos <carua97@gmail.com>
2023-03-26 15:32:30 +0200

    Change all the state for the reducer calendar

--- src/components/calendar/calendar-day.tsx
+++ src/components/calendar/calendar-day.tsx
@@ -1,20 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
 import { useReservations } from "../../hooks/use-reservations";
+import { updateActive, updateDate } from "../../reducer/calendar-slice";
 import { ReservationsRepo } from "../../services/reservation-repo";
-import { ReserveInfo } from "./calendar";
+import { AppDispatch } from "../../store/store";
 
 interface CalendarDayProps {
   day: number | string;
   lastOfMonth: Date;
-  reserveSet: React.Dispatch<React.SetStateAction<Partial<ReserveInfo>>>;
 }
 
-export function CalendarDay({
-  day,
-  lastOfMonth,
-  reserveSet,
-}: CalendarDayProps) {
-  if (day < 1) day = "";
-  if (day > lastOfMonth.getDate()) day = "";
+export function CalendarDay({ day, lastOfMonth }: CalendarDayProps) {
+  const dispatch = useDispatch<AppDispatch>();
 
   const repoReservations = new ReservationsRepo();
   const { reservations } = useReservations(repoReservations);
@@ -24,15 +20,23 @@ export function CalendarDay({
     return splitDate[2];
   });
 
+  if (day < 1) day = "";
+  if (day > lastOfMonth.getDate()) day = "";
+
   const yearMonthDate = `${lastOfMonth.getFullYear()}-${
     lastOfMonth.getMonth() + 1
   }-${day}`;
+
+  const handlerDay = () => {
+    dispatch(updateDate(yearMonthDate));
+    dispatch(updateActive(true));
+  };
 
   return (
     <td>
       <button
         disabled={numberMonth.includes(day.toString())}
-        onClick={() => reserveSet({ date: yearMonthDate, active: true })}
+        onClick={() => handlerDay()}
       >
         {day}
       </button>
--- src/components/calendar/calendar-week.tsx
+++ src/components/calendar/calendar-week.tsx
@@ -1,28 +1,16 @@
-import { ReserveInfo } from "./calendar";
 import { CalendarDay } from "./calendar-day";
 
 interface CalendarWeekProps {
   week: number;
   offset: number;
   lastOfMonth: Date;
-  reserveSet: React.Dispatch<React.SetStateAction<Partial<ReserveInfo>>>;
 }
 
-export function CalendarWeek({
-  week,
-  offset,
-  lastOfMonth,
-  reserveSet,
-}: CalendarWeekProps) {
+export function CalendarWeek({ week, offset, lastOfMonth }: CalendarWeekProps) {
   const days = [];
   for (let i = 0; i < 7; i++) {
     days.push(
-      <CalendarDay
-        day={week * 7 + i + offset + 1}
-        lastOfMonth={lastOfMonth}
-        reserveSet={reserveSet}
-      />
-      // CalendarDay(week * 7 + i + offset + 1, lastOfMonth)
+      <CalendarDay day={week * 7 + i + offset + 1} lastOfMonth={lastOfMonth} />
     );
   }
 
