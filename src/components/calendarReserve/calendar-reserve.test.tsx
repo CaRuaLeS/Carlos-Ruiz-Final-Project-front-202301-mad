47bf311272093bf30d01f8e1ed4662619dd49372
CarlosRuiz <carua97@gmail.com>
2023-03-28 13:14:28 +0200

    Delete unused code and comment code

--- src/components/calendarReserve/calendar-reserve.test.tsx
+++ src/components/calendarReserve/calendar-reserve.test.tsx
@@ -1,35 +1,20 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
 import { act, fireEvent, render, screen } from "@testing-library/react";
-import { Provider, useDispatch, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { useReservations } from "../../hooks/use-reservations";
 import { store } from "../../store/store";
 import { CalendarReserve } from "./calendar-reserve";
 
 jest.mock("../../hooks/use-reservations");
-// jest.mock("react-redux", () => ({
-//   useDispatch: jest.fn(),
-//   useSelector: jest.fn(),
-// }));
 
 describe("Given the CalendarReserve", () => {
   const mockReservationCreate = jest.fn();
-  let usersMock;
-  let mockDispatch = jest.fn();
 
   beforeEach(async () => {
     (useReservations as jest.Mock).mockReturnValue({
       reservationCreate: mockReservationCreate,
     });
-
-    usersMock = {
-      extraInfo: {
-        token: undefined,
-      },
-    };
-    // (useSelector as jest.Mock).mockReturnValue(usersMock);
-    // (useDispatch as jest.Mock).mockReturnValue(mockDispatch);
-
     await act(async () => {
       render(
         <Provider store={store}>
