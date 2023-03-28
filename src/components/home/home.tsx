ab75bbab6b8d359a59bfee5cc6e547461c4a4c36
CarlosRuiz <carua97@gmail.com>
2023-03-28 15:10:02 +0200

    Add home style basic

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,4 +1,4 @@
-import "./home.css";
+import styles from "./home.module.scss";
 import { useMemo } from "react";
 import { useEscapeRooms } from "../../hooks/use-escaperooms";
 import { EscaperoomsRepo } from "../../services/escaperoom-repo";
@@ -9,7 +9,7 @@ export function Home() {
   const { escaperoomGetAll, escaperoomGetByTheme } = useEscapeRooms(repo);
 
   return (
-    <section className="home">
+    <section className={styles.home}>
       <h2 className="home__title">Home</h2>
       <select
         onChange={async (element) => {
@@ -18,7 +18,7 @@ export function Home() {
             ? escaperoomGetAll()
             : escaperoomGetByTheme(selectedTheme);
         }}
-        className="home__select"
+        className={styles.select}
       >
         <option value="All" placeholder="Select a theme">
           All
