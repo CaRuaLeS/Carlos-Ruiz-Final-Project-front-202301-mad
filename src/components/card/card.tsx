a5f7622c93a8d71aabf59c43f879e135cb61fc34
CarlosRuiz <carua97@gmail.com>
2023-03-28 16:26:44 +0200

    Add home css styles

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -10,7 +10,7 @@ type RoomProps = {
 export function Card({ room }: RoomProps) {
   return (
     <li className={styles.card}>
-      <Link to={`/details/${room.id}`}>
+      <Link to={`/details/${room.id}`} className={styles.link}>
         <img
           className={styles.image}
           src={room.images![0]}
@@ -19,9 +19,17 @@ export function Card({ room }: RoomProps) {
         <div className={styles.name}>{room.name}</div>
       </Link>
       <div className={styles.details}>
-        <div>{room.players}</div>
-        <div>Theme: {room.theme}</div>
-        <div>Difficulty: {room.difficulty}</div>
+        <div className={styles.content}>
+          <img src="./images/group.svg" alt="number of player" />
+          {room.players}
+        </div>
+        <div className={styles.content}>
+          <img src="./images/theme.svg" alt="theme" /> {room.theme}
+        </div>
+        <div className={styles.content}>
+          <img src="./images/difficulty.svg" alt="difficulty" />
+          {room.difficulty}
+        </div>
       </div>
     </li>
   );
