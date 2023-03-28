6e98a2c15deccaeb67d1a0d512960c8172992a2c
CarlosRuiz <carua97@gmail.com>
2023-03-28 11:03:47 +0200

    Add calendar test

--- src/components/calendar/calendar.test.tsx
+++ src/components/calendar/calendar.test.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
 import { act, render, screen } from "@testing-library/react";
-import { useDispatch, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { useReservations } from "../../hooks/use-reservations";
+import { store } from "../../store/store";
 import { CalendarReserve } from "../calendarReserve/calendar-reserve";
 import { Calendar } from "./calendar";
 import { CalendarWeek } from "./calendar-week";
@@ -10,28 +11,20 @@ import { CalendarWeek } from "./calendar-week";
 jest.mock("./calendar-week");
 jest.mock("../../hooks/use-reservations");
 jest.mock("../calendarReserve/calendar-reserve");
-jest.mock("react-redux", () => ({
-  useSelector: jest.fn(),
-  useDispatch: jest.fn(),
-}));
 
 describe("Given the Calendar component", () => {
   const mockReservationGetFilterMonth = jest.fn();
-  let mockDispatch = jest.fn();
-  const mockReservation = {
-    active: false,
-    date: "",
-  };
+
   beforeEach(async () => {
     (useReservations as jest.Mock).mockReturnValue({
       reservationGetFilterMonth: mockReservationGetFilterMonth,
     });
-    (useDispatch as jest.Mock).mockReturnValue(mockDispatch);
-
-    (useSelector as jest.Mock).mockReturnValue(mockReservation);
-
     await act(async () => {
-      render(<Calendar monthOffset={0} roomId={"12345"}></Calendar>);
+      render(
+        <Provider store={store}>
+          <Calendar monthOffset={0} roomId={"12345"}></Calendar>
+        </Provider>
+      );
     });
   });
 
@@ -50,15 +43,17 @@ describe("Given the Calendar component", () => {
       (useReservations as jest.Mock).mockReturnValue({
         reservationGetFilterMonth: mockReservationGetFilterMonth,
       });
-      const mockReservation = {
-        active: true,
-        date: "",
-      };
-      (useSelector as jest.Mock).mockReturnValue(mockReservation);
-      (useDispatch as jest.Mock).mockReturnValue(mockDispatch);
-      render(<Calendar monthOffset={0} roomId={"12345"}></Calendar>);
     });
     test("then it should render the CalendarReserve component", () => {
+      store.dispatch({
+        type: "calendar/updateActive",
+        payload: { active: true },
+      });
+      render(
+        <Provider store={store}>
+          <Calendar monthOffset={0} roomId={"12345"}></Calendar>
+        </Provider>
+      );
       expect(CalendarReserve).toHaveBeenCalled();
     });
   });
