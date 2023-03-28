6ef6fb2d4fce93821a835a0bba79917a74466885
CarlosRuiz <carua97@gmail.com>
2023-03-28 11:13:51 +0200

    Fix calendar reserve tests

--- src/components/calendarReserve/calendar-reserve.test.tsx
+++ src/components/calendarReserve/calendar-reserve.test.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-render-in-setup */
 import { act, fireEvent, render, screen } from "@testing-library/react";
-import { useDispatch, useSelector } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import { useReservations } from "../../hooks/use-reservations";
+import { store } from "../../store/store";
 import { CalendarReserve } from "./calendar-reserve";
 
 jest.mock("../../hooks/use-reservations");
-jest.mock("react-redux", () => ({
-  useDispatch: jest.fn(),
-  useSelector: jest.fn(),
-}));
+// jest.mock("react-redux", () => ({
+//   useDispatch: jest.fn(),
+//   useSelector: jest.fn(),
+// }));
 
 describe("Given the CalendarReserve", () => {
   const mockReservationCreate = jest.fn();
@@ -26,11 +27,15 @@ describe("Given the CalendarReserve", () => {
         token: undefined,
       },
     };
-    (useSelector as jest.Mock).mockReturnValue(usersMock);
-    (useDispatch as jest.Mock).mockReturnValue(mockDispatch);
+    // (useSelector as jest.Mock).mockReturnValue(usersMock);
+    // (useDispatch as jest.Mock).mockReturnValue(mockDispatch);
 
     await act(async () => {
-      render(<CalendarReserve />);
+      render(
+        <Provider store={store}>
+          <CalendarReserve />
+        </Provider>
+      );
     });
   });
   describe("when you render the component", () => {
@@ -44,8 +49,11 @@ describe("Given the CalendarReserve", () => {
       const element = screen.getByRole("button");
       fireEvent.click(element);
 
-      expect(useSelector).toHaveBeenCalled();
-      expect(useDispatch).toHaveBeenCalled();
+      expect(mockReservationCreate).toHaveBeenCalled();
+
+      const mockState = store.getState();
+
+      expect(mockState.calendar.active).toEqual(false);
     });
   });
 });
